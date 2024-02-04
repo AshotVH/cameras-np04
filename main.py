63a63fe32d6ccbcf982ad3ab311e769cf3759a1d
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-05 02:26:34 +0400

    05.02.24

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ def index():
 def video_feed(stream_id):
 
     # URL of the internal video stream
-    internal_stream_url = f"https://cam401-np04-slow-control.app.cern.ch/api/stream.mp4?src={stream_id}&mp4=flac"
-    # internal_stream_url = f"http://188.185.78.34:1984/api/stream.mp4?src={stream_id}&mp4=flac"
-    
+    internal_stream_url = f"https://streams-np04-slow-control.app.cern.ch/api/stream.mp4?src={stream_id}&mp4=flac"
+      
 
 
     def generate():
@@ -26,20 +25,5 @@ def video_feed(stream_id):
     return Response(stream_with_context(generate()), content_type='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/movie_feed/<stream_id>')
-def movie_feed(stream_id):
-
-    # URL of the internal video stream
-    # internal_stream_url = f"https://cam401-np04-slow-control.app.cern.ch/api/stream.mp4?src={stream_id}&mp4=flac"
-    internal_stream_url = f"http://188.185.78.34:1984/api/stream.mp4?src={stream_id}&mp4=flac"
-    
-
-
-    def generate():
-        with requests.get(internal_stream_url, stream=True) as r:
-            for chunk in r.iter_content(chunk_size=4096):
-                yield chunk
-
-    return Response(stream_with_context(generate()), content_type='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
