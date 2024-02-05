bfb8079bda64a7d8d0d040ee6175bfd36b1a8be1
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-05 05:56:42 +0400

    05.02.24

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,24 @@
-const options = { autoplay: true };
-const player = videojs("player", options, function onPlayerReady() {
-  this.play();
-  this.on("ended", function () {
-    videojs.log("stream ended");
+window.onload = () => {
+  const options = { autoplay: true };
+  const player = videojs("player", options, function onPlayerReady() {
+    this.play();
+    this.on("ended", function () {
+      videojs.log("stream ended");
+    });
   });
-});
 
-player.on("loadedmetadata", function () {
-  const width = player.videoWidth();
-  const height = player.videoHeight();
-  console.log("Resolution:", width, "x", height);
-});
+  player.on("loadedmetadata", function () {
+    const width = player.videoWidth();
+    const height = player.videoHeight();
+    console.log("Resolution:", width, "x", height);
+  });
+  const videoButtons = document.getElementsByClassName("btn-check");
+  for (let i = 0; i < videoButtons.length; i++) {
+    videoButtons[i].addEventListener("click", function (event) {
+      let streamId = event.target.getAttribute("data-streamid");
+      player.src('/video_feed/' + streamId);
+      console.log(player.src());
+      player.play();
+    });
+  }
+};
