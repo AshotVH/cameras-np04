514e1f0bf925b8db5e030e06c0ad453b46219d51
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-18 03:32:16 +0400

    18.02.24

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def index():
 def video_feed(stream_id):
     
     # internal_stream_url = f"http://137.138.155.45:1984/api/stream.mp4?src={stream_id}&mp4=flac"
-    internal_stream_url = "http://188.185.66.11:8080/"
+    internal_stream_url = "http://188.185.66.11:8080/video.mp4"
 
     
   
