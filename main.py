b8d7e2c19cf4e32dcc11ec4e3f6742c13dd118a7
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-18 15:09:39 +0400

    18.02.24

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def index():
 def video_feed(stream_id):
     
     # internal_stream_url = f"http://137.138.155.45:1984/api/stream.mp4?src={stream_id}&mp4=flac"
-    internal_stream_url = "http://188.185.66.11:8080/"
-
+    # internal_stream_url = "http://188.185.66.11:8080/"
+    internal_stream_url = "https://streams-np04-slow-control.app.cern.ch/api/stream.mp4?src=cartoon-http&mp4=flac"
     
   
     def generate():
