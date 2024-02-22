894e3670adcffe0931717ece6a40ea87ab706f2b
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-23 01:50:18 +0400

    23.02.24

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from flask import Flask, Response, stream_with_context, request, render_template
+from flask import Flask, Response, stream_with_context, request, render_template,redirect, url_for
 import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def root():
+    return redirect(url_for('index', cam='cam401'))
+
+@app.route('/<cam>')
+def index(cam):
+    return render_template('index.html', cam=cam)
 
 
 @app.route('/video_feed/<stream_id>')
 def video_feed(stream_id):
     
-    internal_stream_url = f"https://{stream_id}-np04-slow-control.app.cern.ch/api/stream.mp4?src=video2&mp4=flac"
+    internal_stream_url = f"https://{stream_id}-np04-slow-control.app.cern.ch/api/stream.mp4?src=video&mp4=flac"
     
-  
     def generate():
         with requests.get(internal_stream_url, stream=True) as r:
             for chunk in r.iter_content(chunk_size=4096):
--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,6 @@
 window.onload = () => {
   const options = { autoplay: true, fluid: true };
   const player = videojs("player", options, function onPlayerReady() {
-    player.src({ type: 'video/mp4', src: "/video_feed/cam401" });
     this.play();
-   
   });
-
-  player.on("loadedmetadata", function () {
-    const width = player.videoWidth();
-    const height = player.videoHeight();
-    console.log("Resolution:", width, "x", height);
-  });
-  const videoButtons = document.getElementsByClassName("btn-check");
-  for (let i = 0; i < videoButtons.length; i++) {
-    videoButtons[i].addEventListener("click", function (event) {
-      const streamId = event.target.getAttribute("data-streamid");
-    
-      const streamUrl = '/video_feed/' + streamId;
-      player.src({ type: 'video/mp4', src: streamUrl });
-      
-      console.log(player.src());
-      player.load();
-      player.play();
-    });
-  }
 };
