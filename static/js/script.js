579abf87b2c78917c183e3f2f6ba8240768c954b
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-27 05:23:22 +0400

    stream mode mse

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,3 @@
-window.onload = () => {
-  const options = { autoplay: true, fluid: true };
-  const player = videojs("player", options, function onPlayerReady() {
-    this.play();
-  });
-};
 function onLoadHandler() {
   const iframe = document.getElementById("video_frame");
   const element = iframe.contentWindow.document.getElementsByClassName("info")[0];
